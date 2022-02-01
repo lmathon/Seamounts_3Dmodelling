ce6df475439dcfbafa9153c3e41add5f73e0fd3c
root <root@EB10PA4.newcefe.newage.fr>
2022-02-01 13:48:42 +0100

    format edna and bruvs data

---
+++ 01_formating_data/01_eDNA/03_formate_edna_data.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+load("01_formating_data/01_eDNA/Rdata/02-clean-data.Rdata")
+var <- read.csv("01_formating_data/01_eDNA/eDNA_Environmental_Variables_SEAMOUNTS_November2021.csv", sep=",")
+
+edna <- df_all_filters %>%
+  filter(habitat_type %in% c("Sommet", "Pelagique"))
+
+edna_motus <- edna[,c("code_explo", "definition", "count_reads")]
+
+# Pull PCR replicate
+edna_motus <- edna_motus %>%
+  group_by(.dots = c("code_explo", "definition")) %>% 
+  summarise(n_reads = sum(count_reads)) %>% 
+  ungroup()
+
+# One column per MOTU
+edna_motus <- pivot_wider(edna_motus, names_from="definition", values_from="n_reads", values_fill=NA)
+edna_motus[is.na(edna_motus)] <- 0
+edna_motus[,-1][edna_motus[,-1] >0] <- 1
+
+# Sum of total richness per station
+edna_motus$richness_tot <- rowSums(edna_motus[,c(2:442)])
+
+# join with metadata and variables
+meta <- read.csv("01_formating_data/01_eDNA/EdnaDataForLaetitia.csv", sep=";")
+meta <- meta[,c("Station", "Date", "Time", "Sampling_depth")]
+meta <- meta %>% distinct(Station, .keep_all=T)
+
+edna_motu_variables <- full_join(edna_motus, meta, by=c("code_explo"="Station"))
+edna_motu_variables <- left_join(edna_motu_variables, var, by=c("code_explo"="Station"))
+
+
+# save csv and rdata
+write.csv(edna_motu_variables, "01_formating_data/01_eDNA/edna_motus_variables.csv", row.names = F)
+save(edna_motu_variables, file = "01_formating_data/01_eDNA/Rdata/edna_motus_variables.rdata")
---
+++ 01_formating_data/02_BRUVs/01_formate_bruvs_data.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+bruvs <- read.csv("01_formating_data/02_BRUVs/BruvsDataForLaetitia.csv", sep=";")
+
+bruvs_species <- bruvs[,c("Station", "Species", "MaxN")]
+bruvs_species$MaxN <- as.numeric(bruvs_species$MaxN)
+
+# One column per species
+bruvs_species <- pivot_wider(bruvs_species, names_from="Species", values_from="MaxN", values_fill=NA)
+bruvs_species <- as.data.frame(bruvs_species)
+bruvs_species <- bruvs_species %>% replace(.=="NULL", 0)
+
+# Sum of total abundance per station
+bruvs_species[,c(2:191)] <- as.numeric(unlist(bruvs_species[,c(2:191)]))
+bruvs_species$abundance_tot <- rowSums(bruvs_species[,c(2:191)])
+
+# Join with explanatory variables
+var <- read.csv("01_formating_data/02_BRUVs/Bruvs_Environmental_Variables_SEAMOUNTS_November2021.csv", sep=",")
+
+bruvs_species_variable <- left_join(bruvs_species, var, by="Station")
+
+write.csv(bruvs_species_variable, "01_formating_data/02_BRUVs/Bruvs_species_variables.csv", row.names = F)
+save(bruvs_species_variable, file="01_formating_data/02_BRUVs/Rdata/bruvs_species_variables.rdata")
