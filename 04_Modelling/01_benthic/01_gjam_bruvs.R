c5085d2c91d514aa9e54d5629e5458eb9a98a6bb
root <root@EB10PA4.newcefe.newage.fr>
2022-03-12 11:31:35 +1100

    trials gjam bruvs

--- 04_Modelling/01_benthic/01_gjam_bruvs.R
+++ 04_Modelling/01_benthic/01_gjam_bruvs.R
@@ -1,4 +1,5 @@
 library(gjam)
+library(rsq)
 library(tidyverse)
 library(ecodist)
 library(modelr)
@@ -103,11 +104,12 @@ rmse(lm, df)
 
 
 ######################################################################################################################################
-# Full  model + selected Quadratic
+# Full  model + Quadratic
 formula <- as.formula(~ Habitat+Salinity +SuspendedParticulateMatter + EastwardVelocity + 
-                        NorthwardVelocity + SSTmean + Chla+ BottomDepth + 
-                        TravelTime + ReefMinDist + I(NorthwardVelocity^2) + I(SSTmean^2) + 
-                        I(TravelTime^2))
+                        NorthwardVelocity + SSTmax + Chla+ BottomDepth + 
+                        TravelTime + ReefMinDist + I(Salinity^2) + I(SuspendedParticulateMatter^2) + I(EastwardVelocity^2) + 
+                        I(NorthwardVelocity^2) + I(SSTmax^2) + I(Chla^2) + I(BottomDepth^2) + 
+                        I(TravelTime^2) + I(ReefMinDist^2))
 
 types <- c(rep('DA', ncol(bruvs_species)))
 ml <- list(ng = 500, burnin = 50, typeNames = types)
@@ -137,20 +139,66 @@ lm <- lm(df$pred ~ df$obs)
 rsq(lm)
 rmse(lm, df)
 
-## DIC = 20646.54
+## DIC = 19673.56
 ## xPred pretty good
-## pearson r = 0.54
-## R² = 0.29
-## RMSE = 0.86
+## pearson r = 0.63
+## R² = 0.40
+## RMSE = 0.90
+## 4 species clusters
+## SSTmax better than SSTmean
+
+
+
+
+######################################################################################################################################
+# Full  model + selected Quadratic
+formula <- as.formula(~ Habitat+Salinity +SuspendedParticulateMatter + EastwardVelocity + 
+                        NorthwardVelocity + SSTmax + Chla+ BottomDepth + 
+                        TravelTime + ReefMinDist + I(SSTmax^2))
+
+types <- c(rep('DA', ncol(bruvs_species)))
+ml <- list(ng = 500, burnin = 50, typeNames = types)
+
+# fit model
+gjam <- gjam(formula = formula, xdata = bruvs_var, ydata = bruvs_species, modelList= ml )
+
+# outputs model
+plotPars = list(GRIDPLOTS=T, SAVEPLOTS = T)
+gjamPlot( output = gjam,  plotPars = plotPars)
+
+gjam$fit$DIC
+gjam[["inputs"]][["designTable"]]
+gjam[["parameters"]][["sensTable"]]
+
+yobs <- gjam[["inputs"]][["y"]]
+ypred <- gjam[["prediction"]][["ypredMu"]]
+
+# compute correlation between ypred and yobs (removing "other")
+
+df <- data.frame(obs=as.vector(yobs), pred=as.vector(ypred))
+cor.test(df$pred, df$obs, method = "pearson")
+
+plot(df$pred ~ df$obs)
+
+lm <- lm(df$pred ~ df$obs)
+rsq(lm)
+rmse(lm, df)
+
+## DIC = 17956.94
+## xPred pretty good
+## pearson r = 0.52
+## R² = 0.26
+## RMSE = 0.81
 ## 4 species clusters
 
 
 ######################################################################################################################################
-# Full  Full model + Full Quadratic
-formula <- as.formula(~ Habitat+Salinity +SuspendedParticulateMatter + EastwardVelocity + seafloorTemp +
-                        NorthwardVelocity + SSTmean + Chla+ BottomDepth + SummitDepth + SummitRugosity + SummitAreaKm2 +
-                        TravelTime + ReefMinDist + I(NorthwardVelocity^2) + I(SSTmean^2) + I(seafloorTemp^2) +
-                        I(TravelTime^2) + I(SummitDepth^2) + I(SummitRugosity^2))
+# Full  model + Cubic
+formula <- as.formula(~ Habitat+Salinity +SuspendedParticulateMatter + EastwardVelocity + 
+                        NorthwardVelocity + SSTmax + Chla+ BottomDepth + 
+                        TravelTime + ReefMinDist + I(Salinity^3) + I(SuspendedParticulateMatter^3) + 
+                        I(EastwardVelocity^3) + I(NorthwardVelocity^3) + I(SSTmax^3) + I(Chla^3) + 
+                        I(BottomDepth^3) + I(TravelTime^3) + I(ReefMinDist^3))
 
 types <- c(rep('DA', ncol(bruvs_species)))
 ml <- list(ng = 500, burnin = 50, typeNames = types)
@@ -180,12 +228,15 @@ lm <- lm(df$pred ~ df$obs)
 rsq(lm)
 rmse(lm, df)
 
-## DIC = 20000
+## DIC = 22247.35
 ## xPred pretty good
-## pearson r = 0.64
-## R² = 0.41
-## RMSE = 0.93
+## pearson r = 0.62
+## R² = 0.38
+## RMSE = 0.91
 ## 4 species clusters
+
+
+
 
 ##########################################################################################
 # load new data for predictions
